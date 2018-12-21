baaa58e34bc06427359b560e8eaf3a4b7e76ccbe
Aaron <toddaa@gmail.com>
2018-12-21 15:08:04 -0500

    Eliminates false nav-collapse from triggering on brand click

--- src/components/topbar/index.js
+++ src/components/topbar/index.js
@@ -23,17 +23,22 @@ class Topbar extends Component {
 		super(props);
 
 		this.handleClick = this.handleClick.bind(this);
+		this.handleBrandClick = this.handleBrandClick.bind(this);
 		this.state = {
-			showMenu: false
+			showMenu: false,
+			currentPage:""
 		};
 	}
 	handleClick(){
 		this.setState({showMenu : !this.state.showMenu});
 	}
+	handleBrandClick(){
+		this.setState({showMenu : false});
+	}
 	render() {
 		return (
 				<nav id="topbar" className="navbar navbar-expand-sm navbar-dark bg-dark fixed-top d-md-none">
-					<Link to='/' className="navbar-brand" onClick={this.handleClick}><Avatar src="A736416D-1265-43E8-BD43-EA3061A13A79.JPG" name="Aaron Todd" round={true} size={40} /> Aaron Todd</Link>
+					<Link to='/' className="navbar-brand" onClick={this.handleBrandClick}><Avatar src="A736416D-1265-43E8-BD43-EA3061A13A79.JPG" name="Aaron Todd" round={true} size={40} /> Aaron Todd</Link>
 					<button className="navbar-toggler" type="button" onClick={this.handleClick} data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 						<span className="navbar-toggler-icon"></span>
 					</button>

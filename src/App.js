f25f347dd394c408d7af0afc9b280168c058fc8a
Aaron Todd <toddaa@gmail.com>
2019-01-02 18:36:29 -0500

    Adds Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ReactGA from 'react-ga';
 import './App.css';
 import RootPage from './components/pages/root';
 import AboutPage from './components/pages/about';
 import Error404 from './components/pages/404';
 import Sidebar from './components/sidebar';
 import Topbar from './components/topbar';
+
+
+ReactGA.initialize('UA-131576285-01');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 class App extends Component {
 	render() {

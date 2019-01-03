3a9987cb2f4823434365aa1136afed2341cf70a5
Aaron Todd <toddaa@gmail.com>
2019-01-03 15:38:02 -0500

    New array of subpages are now dependent on environment variable.  JS code will add them only if they are allowed in production

--- src/App.js
+++ src/App.js
@@ -13,21 +13,39 @@ import Topbar from './components/topbar';
 
 ReactGA.initialize('UA-131576285-1');
 ReactGA.pageview(window.location.pathname);
+//console.log(process.env)
+
+const pages = [
+	{name: 'About', path: '/about', component: AboutPage, auth: (process.env.REACT_APP_INC_PAGE_ABOUT === 'true')},
+	{name: 'Work', path: '/work', component: WorkPage, auth: (process.env.REACT_APP_INC_PAGE_WORK === 'true')},
+	{name: 'Blog', path: '/blog', component: BlogPage, auth: (process.env.REACT_APP_INC_PAGE_BLOG === 'true')},
+]
+
+function PageRoutes(props) {
+	let variable = pages.map((page, i) => {
+			if (page.auth || process.env.NODE_ENV === 'development'){
+				//console.log("add route: " + page.name)
+				return <Route key={i} path={page.path} component={page.component} />;
+			}
+		}
+	);
+
+	return <Switch>
+			<Route exact path='/' component={RootPage} />
+			{variable}
+			<Route component={Error404} />
+		</Switch>;
+}
 
 class App extends Component {
 	render() {
 		return (
 			<Router>
 				<div>
-					<Topbar />
+					<Topbar content={pages} />
 					<div className="wrapper">
-						<Sidebar />
-						<Switch>
-							<Route exact path='/' component={RootPage} />
-							<Route path='/about' component={AboutPage} />
-							<Route path='/work' component={WorkPage} />
-							<Route component={Error404} />
-						</Switch>
+						<Sidebar content={pages} />
+						<PageRoutes content={pages}/>
 					</div>
 				</div>
 			</Router>

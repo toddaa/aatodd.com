2e9855a2713ee03ae1fe4d55792ffe20157e3d87
Aaron Todd <toddaa@gmail.com>
2019-01-10 11:24:07 -0500

    Custom messages for the 404 page

--- src/components/pages/404/index.js
+++ src/components/pages/404/index.js
@@ -9,14 +9,23 @@ const iconError = (
 
 class ErrorPage extends Component {
 	render() {
+		const messages_404 = [
+			'Fear is the path to the dark side.  Choose another path.',
+			'Nope.  Nothing here.',
+			'You broke it! Head back and try again.',
+			'No match.  Are you sure you know what you want?',
+			'This is not the page you are looking for... ',
+			'Too soon!  This page isn\'t complete quite yet.'
+		];
+
 		window.scrollTo(0, 0);
 		return (
-			<div className="container-fluid h-100">
+			<div className="container-fluid h-100 content">
 				<div className="row h-100">
 					<div className="col text-center vertical-center">
 						<h1>{iconError}</h1>
-						<h1>- 404 -</h1>
-						<h2>Fear is the path to the dark side.  Choose another path.</h2>
+						{/* <h1>404 - Page Not Found</h1> */}
+						<h3>{messages_404[Math.floor(Math.random() * messages_404.length)]}</h3>
 					</div>
 				</div>
 			</div>

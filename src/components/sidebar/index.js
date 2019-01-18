4d4921d19e6f230feb2794f4b87b3939a1471390
Aaron Todd <toddaa@gmail.com>
2019-01-18 07:53:22 -0500

    Not sure I'll keep this, but it was a fun little expierement

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import { NavLink, Link  } from 'react-router-dom';
 import Avatar from 'react-avatar';
+import classnames from 'classnames';
 import './sidebar.css';
 import Environment from '../environment';
 import Social from '../social';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGalacticRepublic } from '@fortawesome/free-brands-svg-icons';
+import { withRouter } from 'react-router'
+
+const iconSW = (
+	<FontAwesomeIcon icon={faGalacticRepublic} className="fa-6x" />
+);
 
 function SideBarNavButton(props) {
 	const isAllowed = props.isAllowed;
@@ -15,10 +23,31 @@ function SideBarNavButton(props) {
 }
 
 class Sidebar extends Component {
-	// constructor(props) {
-	// 	super(props);
-	// 	//console.log(props)
+	state = {
+		hover_state: false
+	};
+
+	//  constructor(props) {
+	//  	super(props);
+	// // 	//console.log(props)
+    // 	this.flip = this.flip.bind(this);
 	// }
+	componentDidUpdate () {
+		// let { location: { pathname } } = this.props
+
+		// if (prevProps.location.pathname === pathname) return
+		//this.routeChanged()
+		//this.setState({hover_state: false});
+		//this.flip()
+	}
+
+	flip = () => {
+		/*this.setState((prevState) => {
+			return {hover_state: !prevState.hover_state};
+		});*/
+		this.setState({hover_state: true});
+		setTimeout(function(){ this.setState({hover_state: false}); }.bind(this), 1000);
+	}
 	render() {
 		const navContent = this.props.content.map((page, i) =>
 			<SideBarNavButton key={i} text={page.name} path={page.path} onClick={this.toggle} isAllowed={page.auth} />
@@ -26,7 +55,20 @@ class Sidebar extends Component {
 		return (
 				<nav id="sidebar" className="d-none d-md-block d-print-none">
 					<div className="sidebar-header">
-						<Link to='/'><Avatar src="A736416D-1265-43E8-BD43-EA3061A13A79.JPG" name="Aaron Todd" round={true} size={100} style={{margin:'1em'}} /></Link>
+						<Link to='/'>
+							<div className={'flip-container ' + classnames({ hover: this.state.hover_state === true })} onClick={this.flip} >
+								<div className="flipper">
+									<div className="front">
+										<Avatar src="A736416D-1265-43E8-BD43-EA3061A13A79.JPG" name="Aaron Todd" round={true} size={100} style={{margin:'1em'}} />
+									</div>
+									<div className="back">
+										<div style={{display: "inline-block", verticalAlign: "middle", width: "100px", height: "100px", borderRadius: "100%", fontFamily: "Helvetica, Arial, sans-serif", margin: "1em"}}>
+											{iconSW}
+										</div>
+									</div>
+								</div>
+							</div>
+						</Link>
 						<Link to='/'><h4>Aaron Todd</h4></Link>
 					</div>
 

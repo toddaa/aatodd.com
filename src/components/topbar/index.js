c1b871eeb1ee3e47c952019c95df623bedeb5264
Aaron Todd <toddaa@gmail.com>
2019-01-21 14:25:16 -0500

    Mobile hamburger menu button changes to a X when open

--- src/components/topbar/index.js
+++ src/components/topbar/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { NavLink, Link  } from 'react-router-dom';
+import { HamburgerButton } from 'react-hamburger-button';
 import classnames from 'classnames';
 import Avatar from 'react-avatar';
 import Social from '../social';
@@ -22,10 +23,17 @@ class Topbar extends Component {
 		return (
 				<nav id="topbar" className="navbar navbar-expand-sm navbar-dark bg-dark fixed-top d-md-none">
 					<Link to='/' className="navbar-brand" onClick={this.props.miscClick}><Avatar src="A736416D-1265-43E8-BD43-EA3061A13A79.JPG" name="Aaron Todd" round={true} size={40} /> Aaron Todd</Link>
-					<button className="navbar-toggler" type="button" onClick={this.props.hamburgerClick} data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-						<span className="navbar-toggler-icon"></span>
-					</button>
-
+					<div className="navbar-toggler">
+						<HamburgerButton
+							open={this.props.menu_state}
+							onClick={this.props.hamburgerClick}
+							width={20}
+							height={16}
+							strokeWidth={2}
+							color='white'
+							animationDuration={0.3}
+						/>
+					</div>
 					<div className={'navbar-collapse ' + classnames({ collapse: this.props.menu_state === false })} id="navbarSupportedContent">
 						<ul className="navbar-nav mr-auto">
 							{navContent}

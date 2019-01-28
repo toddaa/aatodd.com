7eb19deeed86c0b79d7320d64c661489129eeff2
Aaron Todd <toddaa@gmail.com>
2019-01-28 13:42:58 -0500

    Working on work

--- src/components/pages/work/index.js
+++ src/components/pages/work/index.js
@@ -23,9 +23,31 @@ class AboutPage extends Component {
 				<div className="row h-100">
 					<div className="col-12 col-md-9 offset-md-1">
 						<h1>Work</h1>
-						<p>
+						{/* <p>
 							Yeah, I work, and with the hopes of not boring you with a page simular to my resume I'm going to give you the hightlights
-						</p>
+						</p> */}
+						<section>
+							<h3>The H.T. Hackney Co.</h3>
+							<h6>2017 - Current</h6>
+							<h5>Technology</h5>
+							<p>HTML, CSS, Javascript, PHP, RPG, DB2</p>
+							<h5>Platform</h5>
+							<p>IBM System i, Linux</p>
+							<h5>Goal</h5>
+							<p>To provide customers, associates, and management the tools they need to manage orders and predict future profitibiliy trends.  
+								Corporate level logistics reporting was also on the forefront of my projects.</p>
+						</section>
+						<hr/>
+						<section>
+							<h3>LEIGHTRONIX, INC.</h3>
+							<h6>1999 - 2017</h6>
+							<h5>Technology</h5>
+							<p>HTML, CSS, Javascript, PHP, C, </p>
+							<h5>Platform</h5>
+							<p>Linux</p>
+							<h5>Goal</h5>
+							<p>Provide customers cloud based Video-On-Demand and LIVE video services to increase their viewership reach.</p>
+						</section>
 					</div>
 				</div>
 				<Environment />

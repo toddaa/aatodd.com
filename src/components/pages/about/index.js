7c0e4db7af7de0f63edc54d2b4eb467b464ef0ae
Aaron Todd <toddaa@gmail.com>
2019-02-15 12:22:52 -0500

    Polish

--- src/components/pages/about/index.js
+++ src/components/pages/about/index.js
@@ -24,34 +24,34 @@ class AboutPage extends Component {
 					<div className="col-12 col-md-9 offset-md-1">
 						<h1>About</h1>
 						<p>
-							I'm Aaron, a software developer from the Lansing, Michigan area.
-							I'm consider myself a generalist and I've spent a lot time time learning and getting to know many different technologies.
-							Primarily, my career has revolved around web development.
-							I've worked on many projects ranging from single pages to extreemly large SaaS platforms.
-							Some of which has required me to get very hands on with the underlying infrastructure including IP networking and routing, server hardware, and OS layer software.
-							I've even ported and customized open source software to architechures it wasn't originally intended for.
-							No need to reinvent the wheel if you can simply adapt it right?
-							I've also spent a lot of time focusing on the front end and more importantly the users expierence in regards to the application.
-							I like to create applications that people generally want to use.
+							I'm Aaron, a software developer from the Lansing, Michigan area. 
+							I consider myself a generalist, and I've spent a lot of time learning and getting to know many different technologies. 
+							Primarily, my career has revolved around web development. 
+							I've worked on many projects ranging from single pages to extremely large SaaS platforms. 
+							Some of which has required me to get very hands-on with the underlying infrastructure including IP networking and routing, server hardware, and OS layer software. 
+							I've even ported and customized open source software to architectures they weren’t initially intended for. 
+							No need to reinvent the wheel if you can just adapt it right? 
+							I've also spent a lot of time focusing on the front end and more importantly the user’s experience in regards to the application. 
+							I like to create applications that people want to use.
 						</p>
 						<p>
-							These days I've been focusing a little more on serverless technologies and running code on a cloud platorm.
-							I've developed a real love for single page web apps using framworks such as React and Angular.
-							Combinded with CI/CD build automation tools I've learned that you can create a robust product that people get excited to be a part of.
+							These days I've been focusing a little more on serverless technologies and running code on a cloud platform. 
+							I've developed a real love for single page web apps using frameworks such as React and Angular. 
+							Combined with CI/CD build automation tools I've learned that you can create a robust product that people get excited to be a part of.
 						</p>
 						<p>
-							On top of all that, I love sharing my wealth of knowledge with others.
-							Collaborating not only on how to do things, but also why.
-							I've learned that there are many different ways to do things.
-							I'm willing to share my methods and be open-minded to new.
+							On top of all that, I love sharing my wealth of knowledge with others. 
+							Collaborating not only on how to do things but also why. 
+							I've learned that there are many different ways to do things. 
+							I'm willing to share my methods and be open-minded to alternative methods too. 
 							It's always exciting to mix things up and develop new processes that work best for a team.
 						</p>
 						<p>
-							In my spare time I love spending time with my wife, Alison, and am heavily involved with our three children Samantha, Connor, and Emma.
-							I'm a unit leader to a cub scout group of almost 50 kids.
-							Being an Eagle Scout, I was raised learning how to be a service oriented leader.
-							Jumping back in to those programs for the next generation, in my mind, is a welcome oportunity.
-							Any scouting program will help mold the future of a child, and I want mine to learn to be bold in service to others.
+							In my spare time, I love spending time with my wife, Alison, and am heavily involved with our three children Samantha, Connor, and Emma. 
+							I'm a unit leader to a cub scout group of almost 50 kids. 
+							Being an Eagle Scout, I was raised learning how to be a service-oriented leader. 
+							Jumping back into those programs for the next generation, in my mind, is a welcome opportunity. 
+							Any scouting program will help mold the future of a child, and I want mine to learn to be bold in service to others. 
 							Plus, have all the fun along the way.
 						</p>
 					</div>
